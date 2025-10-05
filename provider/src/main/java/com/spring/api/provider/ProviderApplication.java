b161e30aed24d5c212d1e1552511c7717343a822
javith <javithrasid@gmail.com>
2025-10-05 20:07:13 +0530

    project structure built

--- provider/src/main/java/com/spring/api/provider/ProviderApplication.java
+++ provider/src/main/java/com/spring/api/provider/ProviderApplication.java
@@ -2,8 +2,10 @@ package com.spring.api.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.spring.api")
 public class ProviderApplication {
 
 	public static void main(String[] args) {
